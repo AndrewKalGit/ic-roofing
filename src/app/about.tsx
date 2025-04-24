3c0bb1ad1d54c0a71435611aae3afd42ff737cfe
Andrew Kal <andrewkalgit@gmail.com>
2025-04-24 14:34:40 -0400

    roofing mvp

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -4,23 +4,23 @@ import roofimg2 from "../../public/images/upscaledbeforeafter.webp"
 
 const about = () => {
     return (
- <section className="bg-gray-50 mt-2 lg:mt-14">
+ <section className="bg-gray-50 pt-7">
   <div className="mx-auto max-w-screen-2xl px-4 py-8 sm:px-6 lg:px-8 mb-8">
     <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
       <div className="bg-gray-50 bg-no-repeat bg-cover md:p-12 lg:px-16 lg:py-32 flex flex-col items-center text-center drop-shadow-2xl rounded-xl">
       <div className="p-14 lg:p-0">
-          <h2 className="drop-shadow-2xl text-xl sm:text-3xl lg:text-4xl font-bold xl:pt-4 text-gray-900">
+          <h2 className="drop-shadow-2xl text-lg sm:text-2xl lg:text-4xl font-bold xl:pt-4 text-gray-900">
             Big or Small, We Handle It All
           </h2>
 
-          <p className="text-gray-900 hidden text-xl mx-4 mt-2 sm:mt-4 sm:block">
+          <p className="text-gray-900 text-sm lg:text-xl mx-4 mt-2 sm:mt-4 sm:block">
       We service all of your roofing needs, no matter the size. We specialize in roof repairs, roof maintenance, protective roof coatings, and more.
           </p>
 
           <div className="mt-4 md:mt-8">
         <button className="flex justify-center w-full">
         <a
-          className="rounded-md bg-gray-800 drop-shadow-2xl px-4 py-4 text-xs font-medium text-white shadow-sm hover:bg-gray-800/75 flex justify-center"
+          className="rounded-md bg-gray-800 drop-shadow-2xl px-4 py-4 text-sm font-medium text-white shadow-sm hover:bg-gray-800/75 flex justify-center"
             href="tel:2013970552"
         >
              Get Free Estimate: (201) 397-0552
